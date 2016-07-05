9903939c16d773078d06c2fa319888683751e8dd
WuZhengBin <wzbdroid@gmail.com>
2016-07-05 16:46:07 +0800

    First init

---
+++ MyLocations/MyLocations/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by mxchip on 2016/7/5.
+//  Copyright © 2016年 mxchip. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
